51794bc60545804fe0101a96b779a25a027e84c5
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-04 19:59:00 -0600

    added teensytransfertool and iltms lightsaber code to gitignore, began own code attempt by copying includes

---
+++ lightsaber_attempt/lightsaber_attempt.ino
@@ -0,0 +1,15 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
