1efeb45899ab839d16d958d506fdc9b6c1dbeabb
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-04 20:05:14 -0600

    copied audio setup code from audio system design tool and formatted it

--- lightsaber_attempt/lightsaber_attempt.ino
+++ lightsaber_attempt/lightsaber_attempt.ino
@@ -4,6 +4,19 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+// GUItool: begin automatically generated code
+// https://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog#
+AudioPlaySerialflashRaw  playSwingRaw;
+AudioPlaySerialflashRaw  playHumRaw;
+AudioPlaySerialflashRaw  playImpactRaw;
+AudioMixer4              mixer;
+AudioOutputAnalog        dac;
+AudioConnection          patchCord1(playSwingRaw, 0, mixer, 2);
+AudioConnection          patchCord2(playHumRaw, 0, mixer, 1);
+AudioConnection          patchCord3(playImpactRaw, 0, mixer, 0);
+AudioConnection          patchCord4(mixer, dac);
+// GUItool: end automatically generated code
+
 void setup() {
   // put your setup code here, to run once:
 
